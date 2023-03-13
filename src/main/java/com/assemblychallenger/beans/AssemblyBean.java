4acc6a4a2bbd2e4dca05c0d36049b519858d0009
Rafael Aguiar <rafael.aguiar.im@gmail.com>
2023-03-13 02:08:19 -0300

    Correções e Melhorias dos métodos do ManegedBean, remoção de métodos e variáveis que foram colocadas no AssemblyBO

--- src/main/java/com/assemblychallenger/beans/AssemblyBean.java
+++ src/main/java/com/assemblychallenger/beans/AssemblyBean.java
@@ -1,11 +1,6 @@
 package com.assemblychallenger.beans;
 
-import com.assemblychallenger.business.ConversationListFromFile;
-import com.assemblychallenger.controller.AssemblyController;
-import com.assemblychallenger.dao.AssemblyDao;
-import com.assemblychallenger.dao.AssemblyDao;
-import com.assemblychallenger.dao.LinhasDeMontagensDao;
-import com.assemblychallenger.infra.JPAUtil;
+import com.assemblychallenger.business.AssemblyBO;
 import com.assemblychallenger.models.LinhasDeMontagens;
 import jakarta.inject.Named;
 import com.assemblychallenger.models.Assembly;
@@ -15,14 +10,9 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 
 @Named
 @ManagedBean
@@ -30,75 +20,30 @@ import java.util.List;
 public class AssemblyBean {
 
     public AssemblyBean() {
-        //init();
         preRenderView();
     }
 
-   // @PostConstruct
     public void init() {
         preRenderView();
     }
-    public void preRenderView() {
-        linhasDeMontagens = findAllAssemblysLinhasDeMontagens() ;
-    }
+    public void preRenderView() {linhasDeMontagens = assemblyBO.findAllAssemblysLinhasDeMontagens();}
 
-    private List<Assembly> findAllAssemblys() {
-        return a.findAllAssemblies();
-    }
-    private List<LinhasDeMontagens> findAllAssemblysLinhasDeMontagens() {
-        return linhasDeMontagensDao.findAll();
-    }
-    Integer number = 0;
-    String caminho;
-    List<LinhasDeMontagens> linhasDeMontagens = new ArrayList<>();
-    List<Assembly> assemblyList = new ArrayList<>();
     Assembly assembly = new Assembly();
-    ConversationListFromFile c = new ConversationListFromFile();
-    AssemblyController a = new AssemblyController();
-    @PersistenceContext
-    private EntityManager em = JPAUtil.getEntityManager();
-    LinhasDeMontagensDao linhasDeMontagensDao = new LinhasDeMontagensDao(em);
-    AssemblyDao assemblyDaoImp = new AssemblyDao(em);
+    List<LinhasDeMontagens> linhasDeMontagens = new ArrayList<>();
+
+    AssemblyBO assemblyBO = new AssemblyBO();
 
     public List<LinhasDeMontagens> getLinhasDeMontagens() { return linhasDeMontagens; }
     public void setLinhasDeMontagens(List<LinhasDeMontagens> linhasDeMontagens) { this.linhasDeMontagens = linhasDeMontagens; }
     public Assembly getAssembly() { return assembly; }
-    public List<Assembly> getAssemblyList() { return assemblyList; }
-    public void setAssemblyList(List<Assembly> assemblyList) {
-        this.assemblyList = assemblyList;
-    }
-    public String getCaminho() {
-        return caminho;
-    }
-    public void setCaminho(String caminho) {
-        this.caminho = caminho;
-    }
 
-    public void populaInput() throws Exception {
 
-        if (c.getConversationListFromFile(caminho).isEmpty()){
-            setLinhasDeMontagens(a.findAllMontagens());
-            setAssemblyList(a.findAllAssemblies());
-            em.close();
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro!", "O arquivo não contém um conteúdo válido, seu conteúdo deve conter uma descrição da atividade e o tempo medido em minutos"));
-        }else {
-            number++;
-            a.populaMontagens(number);
-            c.getConversationListFromFile(caminho);
-            a.verificaLinha(c.getConversationList());
-            setLinhasDeMontagens(a.findAllMontagens());
-            setAssemblyList(a.findAllAssemblies());
-            em.close();
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "Arquivo enviado com sucesso!"));
-            caminho = null;
-        }
-    }
     public String limparDados(){
-        assemblyDaoImp.excluirTodos();;
-        linhasDeMontagensDao.excluirTodos();
+        assemblyBO.limparDados();
         preRenderView();
         return "/index.xhtml?faces-redirect=true";
     }
+
     private UploadedFile file;
     public UploadedFile getFile() {
         return file;
@@ -106,6 +51,7 @@ public class AssemblyBean {
     public void setFile(UploadedFile file) {
         this.file = file;
     }
+
     public String upload() {
         try {
             if (file == null){
@@ -116,16 +62,7 @@ public class AssemblyBean {
                 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro!", "Selecione um arquivo do tipo TXT."));
                 return "";
             }
-            byte[] conteudo = file.getContent();
-            File arquivoTemporario = File.createTempFile("arquivo", ".tmp");
-            FileOutputStream saida = new FileOutputStream(arquivoTemporario);
-            saida.write(conteudo);
-            saida.close();
-            String caminhoCompleto = arquivoTemporario.getAbsolutePath();
-            caminho = caminhoCompleto;
-            populaInput();
-
-
+            assemblyBO.upload(file);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (Exception e) {
